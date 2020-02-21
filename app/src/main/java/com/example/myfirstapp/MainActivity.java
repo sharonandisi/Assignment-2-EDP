5a7900cc173aee1b5d6eaa120730ac0dbb44df44
sharonandisi <sharonandisi.28@gmail.com>
2020-02-21 07:31:57 +0300

    first commit

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container_a, new FragmentA())
+                .add(R.id.container_b, new FragmentB())
+                .commit();
+    }
+}
